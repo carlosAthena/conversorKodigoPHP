9bc78ba696146dcf5e0f0348e5798ad78c3f22fa
Carlos Mancía <carlos.mancia@athena.sv>
2023-04-20 07:53:05 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Calculadora de Unidades</title>
+    <link rel="icon" type="image/x-icon" href="./img/K.jpg" />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <section class="sec">
+      <header>
+        <a href="./index.php"><img src="./img/Kodigo.png" class="logo" /></a>
+        <ul>
+          <li><a href="./index.php">Home</a></li>
+          <li><a href="./tiempo/index.php">Tiempo</a></li>
+          <li><a href="./volumen/index.php">Volumen</a></li>
+          <li><a href="./longitud/index.php">Longitud</a></li>
+          <li><a href="./masa/index.php">Masa</a></li>
+          <li><a href="./moneda/index.php">Moneda</a></li>
+          <li><a href="./temperatura/index.php">Temperatura</a></li>
+        </ul>
+      </header>
+      <div class="content">
+        <div class="textBox">
+          <h2>Calculadora de 5 Unidades de<br /><span>MEDIDA</span></h2>
+          <p>
+            Esta Aplicación Web escrita en PHP facilita conversiones de unidades de una manera manera muy fácil y amigable con el usuario,
+            además en su código fuente incorpora los 5 Principios Solid y así como buenas prácticas
+          </p>
+        </div>
+        <div class="imageBox">
+          <img src="./img/Kodigo.png" class="kodigo" />
+        </div>
+      </div>
+  </body>
+</html>
